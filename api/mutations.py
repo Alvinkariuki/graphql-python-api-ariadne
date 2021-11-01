cfb8ed81475514940f0a46f60cc7169746614540
Alvin Kariuki <alvinkariuki230@gmail.com>
2021-11-01 23:04:57 +0300

    Update mutation

--- api/mutations.py
+++ api/mutations.py
@@ -22,3 +22,23 @@ def create_post_resolver(obj, info, title, description):
         }
     
     return payload
+
+
+@convert_kwargs_to_snake_case
+def update_post_resolver(obj, info, title, description):
+    try:
+        post = Post.query.get(id)
+        if post:
+            post.title = title
+            post.description = description
+        db.session.add(post)
+        db.session.commit()
+        payload = {
+            "success":True,
+            "post": post.to_dict()
+        }
+    except AttributeError:
+        payload = {
+            "success": False,
+            "errors": ["item matching id {id} not found"]
+        }
